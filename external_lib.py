5a0cb490455e4659540249b43b6d3b45aa76223f
Nicholas Schrock <schrockn@elementl.com>
2023-09-11 17:19:31 -0400

    cp

--- external_lib.py
+++ external_lib.py
@@ -1,9 +1,11 @@
-from typing import Optional
+from typing import Mapping, Optional
 
 from dagster import AssetMaterialization, DagsterInstance
 from dagster._core.definitions.asset_spec import AssetSpec
 from dagster._core.definitions.assets import AssetsDefinition
 from dagster._core.definitions.decorators.asset_decorator import asset
+from dagster._core.definitions.events import CoercibleToAssetKey
+from dagster._core.definitions.metadata import MetadataValue
 from dagster._core.events import DagsterEvent, DagsterEventType, StepMaterializationData
 
 
@@ -28,8 +30,11 @@ def create_unmanaged_asset(asset_spec: AssetSpec) -> AssetsDefinition:
 # Right now this hits the DagsterInstance directly, but we would
 # change this to hit the Dagster GraphQL API or some sort of ext-esque channel
 def report_asset_materialization(
-    asset_key: str,
-    metadata: dict,
+    asset_key: CoercibleToAssetKey,
+    metadata: Optional[Mapping[str, MetadataValue]] = None,
+    description: Optional[str] = None,
+    partition: Optional[str] = None,
+    tags: Optional[Mapping[str, str]] = None,
     instance: Optional[DagsterInstance] = None,
     run_id: Optional[str] = None,
     job_name: Optional[str] = None,
@@ -38,7 +43,13 @@ def report_asset_materialization(
     dagster_event = DagsterEvent.from_external(
         event_type=DagsterEventType.ASSET_MATERIALIZATION,
         event_specific_data=StepMaterializationData(
-            AssetMaterialization(asset_key=asset_key, metadata=metadata)
+            AssetMaterialization(
+                asset_key=asset_key,
+                metadata=metadata,
+                description=description,
+                tags=tags,
+                partition=partition,
+            ),
         ),
         job_name=job_name,
     )
