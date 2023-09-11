d145a8809c2ec08ecd576ce7feb8e3be61c65c62
Nicholas Schrock <schrockn@elementl.com>
2023-09-11 09:08:26 -0400

    cp

--- external_assets_hello_world.py
+++ external_assets_hello_world.py
@@ -9,4 +9,10 @@ from external_lib import create_externally_computed_asset
 external_one_asset = AssetSpec(asset_key="external_one")
 external_two_asset = AssetSpec(asset_key="external_two", deps=[external_one_asset])
 external_job = define_asset_job("job_representing_external_compute")
-defs = Definitions(assets=[create_externally_computed_asset(external_one_asset), create_externally_computed_asset(external_two_asset)], jobs=[external_job])
+defs = Definitions(
+    assets=[
+        create_externally_computed_asset(external_one_asset),
+        create_externally_computed_asset(external_two_asset)
+    ],
+    jobs=[external_job],
+)
